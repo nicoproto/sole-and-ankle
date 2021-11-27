ed117cc309cee17f1e1d5dae4b1e35878e26a374
Nico Proto <nicolasproto@gmail.com>
2021-11-27 16:39:16 +0100

    Finished ShoeIndex component styling

--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -42,11 +42,21 @@ const ShoeIndex = ({ sortId, setSortId }) => {
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+`;
 
-const LeftColumn = styled.div``;
+const LeftColumn = styled.div`
+  flex: 1;
+  position: sticky;
+  top: 32px;
+  align-self: flex-start;
+`;
 
-const MainColumn = styled.div``;
+const MainColumn = styled.div`
+  flex: 3;
+`;
 
 const Header = styled.header``;
 
